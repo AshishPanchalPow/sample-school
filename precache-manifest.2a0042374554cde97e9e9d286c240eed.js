f9246ed0930c40034974f19459250d7ac6d0982d
Ashish PANCHAL <ashish.panchal@amadeus.com>
2021-06-11 19:02:17 +0530

    Updates

---
+++ precache-manifest.2a0042374554cde97e9e9d286c240eed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fb1115663c525c2dc67ee96072c8e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b33ab11456b4929a55b",
+    "url": "/static/css/2.724cf544.chunk.css"
+  },
+  {
+    "revision": "a9f9f801f75f4e0d8b47",
+    "url": "/static/css/main.4bc6c616.chunk.css"
+  },
+  {
+    "revision": "3b33ab11456b4929a55b",
+    "url": "/static/js/2.67818d63.chunk.js"
+  },
+  {
+    "revision": "aa94962c6278bf8f6df236d7f39cf3dc",
+    "url": "/static/js/2.67818d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f9f801f75f4e0d8b47",
+    "url": "/static/js/main.be675d29.chunk.js"
+  },
+  {
+    "revision": "73237cc1df829a1706ee",
+    "url": "/static/js/runtime-main.9f870c75.js"
+  },
+  {
+    "revision": "eadd02165d4867c1e409f5fc9b0d5062",
+    "url": "/static/media/bug_fixed.eadd0216.png"
+  },
+  {
+    "revision": "46fbb8fed206d871178c94352e36234e",
+    "url": "/static/media/company_team.46fbb8fe.jpg"
+  },
+  {
+    "revision": "4d813d7f5f639172ced0ab93b029b3af",
+    "url": "/static/media/mobile_phone.4d813d7f.png"
+  },
+  {
+    "revision": "1df9976665b11767a893668f678beb44",
+    "url": "/static/media/profile_picture_1.1df99766.jpg"
+  },
+  {
+    "revision": "d4699a1b5f66fac58c44314f5fc5c735",
+    "url": "/static/media/profile_picture_2.d4699a1b.jpg"
+  },
+  {
+    "revision": "3b5461830a0b766b76663e0cd85923ae",
+    "url": "/static/media/profile_picture_3.3b546183.jpg"
+  },
+  {
+    "revision": "12f741d001d38dd2cc3abe504d773627",
+    "url": "/static/media/rocket_launch_.12f741d0.png"
+  },
+  {
+    "revision": "b443ed265359464bafa8e9b61eb371b0",
+    "url": "/static/media/web_development_.b443ed26.png"
+  }
+]);
